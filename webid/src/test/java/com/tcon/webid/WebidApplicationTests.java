0502b5775c365237e85e8cfa45e13220b09bec19
dhanunjay20 <dhanunjay@hiresoftsolutions.com>
2025-11-14 14:44:55 +0530

    initial commit

---
+++ webid/src/test/java/com/tcon/webid/WebidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcon.webid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
